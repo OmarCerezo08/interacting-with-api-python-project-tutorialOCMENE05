33434d3354a77ba5edec657eed928c0d17b45fef
OmarCerezo08 <cerezo0408@gmail.com>
2026-01-06 04:46:49 +0000

    Completed exercises

--- src/app.py
+++ src/app.py
@@ -6,5 +6,6 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
-
-# Spotify API credentials
+# Get credential values
+client_id = os.environ.get("e246c06be2ef4d96b0a7c89b9e2a8189")
+client_secret = os.environ.get("90d70000d68e41188c47a17af29e703e")
